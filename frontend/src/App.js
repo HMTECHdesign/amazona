b623b16fe1a9ec39dda4c41fcb30162ef0d45148
HMTECHdesign <106014841+HMTECHdesign@users.noreply.github.com>
2022-05-30 02:02:39 -0700

    updated version, as at this stage, The web is goodenough upto sign in page. No databse yet.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,64 @@
 import React from 'react';
-import data from './data';
-import Product from './Components/Product';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {useContext } from 'react';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/esm/Badge';
+import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+//import PurchaseScreen from './screens/PurchaseScreen';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">amazona</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
+    <BrowserRouter>
+    <div className="d-flex flex-column site-container">
+      <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazona</Navbar.Brand>
+              </LinkContainer>
+              
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+
       </header>
       <main>
-        <div>
-          <div className="row center">
-            {
-              data.products.map(product =>(
-                <Product key={product._id} product ={product}></Product>
-              ))
-            }
-          </div>
-        </div>
+      <Container className="mt-3">            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              
+              
+            </Routes>
+          </Container>
       </main>
-      <footer className="row center">All right reserved @ HM TECH</footer>
+      <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
     </div>
+  </BrowserRouter>
   );
 }
+
+
 
 export default App;
